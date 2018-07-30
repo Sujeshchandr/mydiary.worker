d72ffa0255d77f2cb7f7b8fbfab5885366701e74
Administrator <Administrator@USER-PC>
2018-07-30 22:32:49 +0530

    Commit MyDiary.Worker Project to GIT

---
+++ src/MyDiary.Worker/worker.js
@@ -0,0 +1,23 @@
+const { Client, logger } = require('camunda-external-task-client-js');
+
+// configuration for the Client:
+//  - 'baseUrl': url to the Process Engine
+//  - 'logger': utility to automatically log important events
+const config = { baseUrl: 'http://localhost:8080/engine-rest', use: logger };
+
+// create a Client instance with custom configuration
+const client = new Client(config);
+
+// susbscribe to the topic: 'charge-card'
+client.subscribe('charge-card', async function({ task, taskService }) {
+  // Put your business logic here
+
+  // Get a process variable
+  const amount = task.variables.get('amount');
+  const item = task.variables.get('item');
+
+  console.log(`Charging credit card with an amount of ${amount}€ for the item '${item}'...`);
+
+  // Complete the task
+  await taskService.complete(task);
+});
